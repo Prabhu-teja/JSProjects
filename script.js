8a73f831a5e3da538ee9575919a0f6c856c0e656
Prabhu_teja <53848058+Prabhu-teja@users.noreply.github.com>
2020-09-17 17:27:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const search_box=document.getElementById("search-box");
+
+search_box.addEventListener("keyup",()=>{
+    search();
+});
+function search(){
+    const projects=document.getElementsByClassName("projects")[0];
+    var a=projects.getElementsByClassName("project");
+    const input=search_box.value;
+    for(var i=0;i<a.length;i++){
+        if(a[i].innerText.toLowerCase().indexOf(input.toLowerCase())>=0){
+            a[i].style.display="inherit";
+        }
+        else{
+            a[i].style.display="none";
+        }
+    }
+}
