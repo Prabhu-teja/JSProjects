baafb9ca15953e1e855362e9ca3d3f5c756dc114
Prabhu_teja <53848058+Prabhu-teja@users.noreply.github.com>
2020-09-19 19:47:12 +0530

    Add files via upload

--- Timer/script.js
+++ Timer/script.js
@@ -1,34 +1,30 @@
-
-let start=prompt("Enter time to start the counter in minutes");
-let time=start*60;
+var submit=document.getElementById("start");
 
 function btn(){
-    
-    var btn =document.getElementById("btn");
+    var btn=document.getElementById("btn");
     btn.addEventListener("click",()=>{
         document.getElementById("message").style.display="none";
     });
-
 }
-
 btn();
+submit.addEventListener("click",timer);
+function timer(){
+    var start=document.getElementById("counter").value;
+    //console.log("STARTED");
+    var time=start*60;
+    var countdown=document.getElementById("countdown");
+    var func=setInterval(()=>{
+        if(start<=0){
+            //console.log("ENDED");
+            document.getElementById("message").style.display="";
+            clearInterval(func);
+        }
+        var minutes=Math.floor(time/60);
+        var seconds=time%60;
+        seconds=seconds<10?"0"+seconds:seconds;
+        minutes=minutes<10?"0"+minutes:minutes;
+        countdown.innerHTML=minutes+":"+seconds;
+        time--;
 
-const countdown=document.getElementById("countdown");
-start = start<10 ? "0"+start : start;
-countdown.innerHTML=start+":00";
-var timer = setInterval(update,1000);
-
-function update(){
-    if(time<0){
-        document.getElementById("message").style.display="";
-        clearInterval(timer);
-        return;
-    }
-    let minutes=Math.floor(time/60);
-    console.log(minutes);
-    let seconds=time%60;
-    seconds=seconds<10 ? "0"+seconds :seconds;
-    minutes=minutes<10 ? "0"+minutes : minutes;
-    countdown.innerHTML=minutes+":"+seconds;
-    time--;
+    },1000);
 }
