ca34ad5b389a87bd8d78968adcbb2bfed10c882e
Prabhu_teja <53848058+Prabhu-teja@users.noreply.github.com>
2020-09-23 12:18:25 +0530

    Add files via upload

--- Timer/script.js
+++ Timer/script.js
@@ -20,17 +20,25 @@ function timer(){
     var time=start*60;
     var countdown=document.getElementById("countdown");
     var func=setInterval(()=>{
-        if(time<=0){
-            //console.log("ENDED");
-            document.getElementById("message").style.display="";
-            clearInterval(func);
-        }
+        
+        enter.readOnly=true;
+        submit.style.pointerEvents="none";
+        enter.style.cursor="not-allowed";
         var minutes=Math.floor(time/60);
         var seconds=time%60;
         seconds=seconds<10?"0"+seconds:seconds;
         minutes=minutes<10?"0"+minutes:minutes;
         countdown.innerHTML=minutes+":"+seconds;
         time--;
+        if(time<0){
+            //console.log("ENDED");
+            document.getElementById("message").style.display="";
+            enter.readOnly=false;
+            submit.style.pointerEvents="";
+            enter.style.cursor="text";
+
+            clearInterval(func);
+        }
 
     },1000);
 }
